be96219115041732fb0884835b789a83a1ea3e15
MemoryMhou <memorymhou1@gmail.com>
2024-02-06 03:53:15 +0200

    try-catch blocks and create custom error types.

--- iOS_Concepts.playground/Contents.swift
+++ iOS_Concepts.playground/Contents.swift
@@ -1,3 +1,20 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+//Implement try-catch blocks and create custom error types.
+enum CustomError: Error {
+    case divisionByZero
+}
+
+func divide(_ a: Int, _ b: Int) throws -> Int {
+    guard b != 0 else {
+        throw CustomError.divisionByZero
+    }
+    return a / b
+}
+
+
+do {
+    print(try divide(8, 0))
+} catch {
+    print("Error: \(error)")
+}
