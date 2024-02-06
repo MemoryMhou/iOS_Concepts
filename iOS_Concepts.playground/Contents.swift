edf846415b1952744b7f139db29491e20a875617
MemoryMhou <memorymhou1@gmail.com>
2024-02-06 04:21:22 +0200

    throws and rethrows in functions.

--- iOS_Concepts.playground/Contents.swift
+++ iOS_Concepts.playground/Contents.swift
@@ -1,3 +1,28 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+//Use throws and rethrows in functions.
+enum MathError: Error {
+    case negativeNumber
+}
+
+func square(_ number: Int) throws -> Int {
+    guard number >= 0 else {
+        throw MathError.negativeNumber
+    }
+    return number * number
+}
+
+func processNumbers( numbers: [Int], using closure: (Int) throws -> Int) rethrows {
+    try numbers.forEach { num in
+        try print(closure(num))
+    }
+}
+
+do {
+    try processNumbers(numbers: [1, 4, 2, -9]) { num in
+        try square(num)
+    }
+} catch {
+    print("Error: \(error)")
+}
+
