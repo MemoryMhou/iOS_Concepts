1c33b5023b2e74e4d2701cbeec8f6e8fbad8bb8e
MemoryMhou <memorymhou1@gmail.com>
2024-02-06 04:36:19 +0200

    complex function incorporating both concepts.

--- iOS_Concepts.playground/Contents.swift
+++ iOS_Concepts.playground/Contents.swift
@@ -1,3 +1,21 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+//complex function incorporating both concepts.
+func calculateResult(numbers: [Int], operation: (Int) throws -> Int) -> [Int] {
+    var results = [Int]()
+    
+    for num in numbers {
+        do {
+            results.append(try operation(num))
+        } catch {
+            print("Error: \(error)")
+        }
+    }
+    
+    return results
+}
+
+let squaredNumbers = calculateResult(numbers: [2, 5, 8, 10]) { $0 * $0 }
+
+print(squaredNumbers)
+
