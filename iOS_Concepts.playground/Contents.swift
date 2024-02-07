9ebf9ef9b0a7fe93b53ef249ef2787a207bbe64c
MemoryMhou <memorymhou1@gmail.com>
2024-02-07 16:45:36 +0200

    functions_and_error_handling

--- iOS_Concepts.playground/Contents.swift
+++ iOS_Concepts.playground/Contents.swift
@@ -1,3 +1,35 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+// functions and errors
+enum PasswordError: Error {
+    case short, obvious
+}
+
+func checkPassword(password: String) throws -> String {
+    if password.count < 5 {
+        throw PasswordError.short
+    }
+    if password == "12345" {
+        throw PasswordError.obvious
+    }
+    if password.count < 8 {
+        return "OK"
+    } else if password.count < 10 {
+        return "Good"
+    } else {
+        return "Excellent"
+    }
+}
+
+do {
+    let string = "12345"
+    
+    let result = try checkPassword(password: string)
+    print("Password rating: \(result)")
+} catch PasswordError.short {
+    print("Please use a longer password.")
+} catch PasswordError.obvious {
+    print("This is an obvious password.")
+} catch {
+    print("There was an error.")
+}
