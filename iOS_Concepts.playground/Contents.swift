39e5372339c74a1153b22b1355e9b7c96cd0f507
MemoryMhou <memorymhou1@gmail.com>
2024-02-09 17:08:23 +0200

    protocols&oriented_programming

--- iOS_Concepts.playground/Contents.swift
+++ iOS_Concepts.playground/Contents.swift
@@ -1,3 +1,32 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+protocol Houses {
+    var numberOfRooms: Int { get }
+    var location: String { get }
+    var bedrooms: Int { get }
+}
+
+extension Houses {
+//    var bathrooms: Int {get}
+}
+
+class House: Houses {
+    var numberOfRooms: Int
+    var location: String
+    var bedrooms: Int
+    var bathrooms: Int
+
+    init(numberOfRooms: Int, location: String, bedrooms: Int, bathrooms: Int) {
+        self.location = location
+        self.numberOfRooms = numberOfRooms
+        self.bedrooms = bedrooms
+        self.bathrooms = bathrooms
+        
+    }
+}
+
+let myhouse = House(numberOfRooms: 3, location: "makoni", bedrooms: 14, bathrooms: 2)
+
+print(myhouse)
+
+
