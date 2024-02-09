054a1a2e0bf5913acd6ad5c382fe0ec5cebd5dc8
MemoryMhou <memorymhou1@gmail.com>
2024-02-09 05:48:16 +0200

    Defining_and_implementing_basic_protocols

--- iOS_Concepts.playground/Contents.swift
+++ iOS_Concepts.playground/Contents.swift
@@ -1,3 +1,45 @@
-import UIKit
+import Foundation
+// Define and implement basic protocols.
 
-var greeting = "Hello, playground"
+//defining the protocol for family
+protocol family{
+    var name: String { get}
+    var age: Int { get}
+    
+    func introduce()
+}
+
+//implementing the protocol for mother
+struct mother: family {
+    let name: String
+    let age: Int
+    
+    init(name: String, age: Int){
+        self.name = name
+        self.age = age
+    }
+    func introduce() {
+        print ("Hie, i am the \(name), of the family, i am \(age) years old" )
+               }
+            }
+ 
+//implementing the protocol for  child
+struct child: family {
+    let name: String
+    let age: Int
+    
+    init(name: String, age: Int){
+        self.name = name
+        self.age = age
+    }
+    func introduce() {
+        print ("Hie, i am the \(name), of the family, i am \(age) years old" )
+    }
+}
+
+
+let Memory = mother(name: "mother", age: 41)
+let Courteny = child(name: "firstBorn", age: 4)
+
+Memory.introduce()
+Courteny.introduce()
