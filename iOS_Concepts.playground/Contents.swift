59bd6de4fb67273e8283ee160c232a9cd59e0404
MemoryMhou <memorymhou1@gmail.com>
2024-02-22 00:45:52 +0200

    initial

--- iOS_Concepts.playground/Contents.swift
+++ iOS_Concepts.playground/Contents.swift
@@ -1,4 +1,15 @@
-import UIKit
+import Foundation
 
-
-var greeting = "Hello, playground"
+class StandardCalculatorController {
+    var model = StandardCalculatorModel()
+    var view = StandardCalculatorView()
+    
+    func calculate(a: Int, b: Int, operation: String) {
+        if operation == "/" && b == 0 {
+            view.displayError()
+        } else {
+            let result = model.calculate(a: a, b: b, operation: operation)
+            view.displayResult(result: result)
+        }
+    }
+}
