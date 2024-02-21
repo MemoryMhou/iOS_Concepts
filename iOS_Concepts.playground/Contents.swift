efeccdc484b6b4499f3bbd5ad1d5c71f87e42e75
MemoryMhou <memorymhou1@gmail.com>
2024-02-21 21:20:47 +0200

    feature/CCI26_model_for_a_simple_calculator_using_MVC

--- iOS_Concepts.playground/Contents.swift
+++ iOS_Concepts.playground/Contents.swift
@@ -1,3 +1,25 @@
+import Foundation
 import UIKit
 
-var greeting = "Hello, playground"
+class StandardCalculatorModel {
+    
+    func calculate(a: Int, b: Int, operation: String) -> Int {
+        switch operation {
+        case "+":
+            return a + b
+        case "-":
+            return a - b
+        case "*":
+            return a * b
+        case "/":
+            if b != 0 {
+                return a / b
+            } else {
+                
+                return Int.min
+            }
+        default:
+            return 0
+        }
+    }
+}
