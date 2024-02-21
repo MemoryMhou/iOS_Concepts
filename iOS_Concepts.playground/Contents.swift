4485b532808e50ee8c13e94d2d43c3ef52b7d5fd
MemoryMhou <memorymhou1@gmail.com>
2024-02-22 00:52:21 +0200

    initial

--- iOS_Concepts.playground/Contents.swift
+++ iOS_Concepts.playground/Contents.swift
@@ -1,4 +1,54 @@
 import UIKit
+import Foundation
 
 
-var greeting = "Hello, playground"
+//Mark -  Model
+class StandardCalculatorModel {
+    func calculate(a: Int, b: Int, operation: String) -> Int {
+        switch operation {
+        case "+":
+            return a + b
+        case "-":
+            return a - b
+        case "*":
+            return a * b
+        case "/":
+            if b != 0 {
+                return a / b
+            } else {
+                return Int.min
+            }
+        default:
+            return 0
+        }
+    }
+}
+
+
+//Mark - View
+class StandardCalculatorView {
+    func displayResult(result: Int) {
+        print("Result: \(result)")
+    }
+    
+    func displayError() {
+        print("math error")
+    }
+}
+
+
+//Mark - Controller
+
+class StandardCalculatorController {
+    var model = StandardCalculatorModel()
+    var view = StandardCalculatorView()
+    
+    func calculate(a: Int, b: Int, operation: String) {
+        if operation == "/" && b == 0 {
+            view.displayError()
+        } else {
+            let result = model.calculate(a: a, b: b, operation: operation)
+            view.displayResult(result: result)
+        }
+    }
+}
