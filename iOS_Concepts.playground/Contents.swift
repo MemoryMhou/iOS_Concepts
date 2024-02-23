36e39baaa4c3c4dcc2524ca87bc6ffeeebad1039
MemoryMhou <memorymhou1@gmail.com>
2024-02-23 05:49:06 +0200

    initial

--- iOS_Concepts.playground/Contents.swift
+++ iOS_Concepts.playground/Contents.swift
@@ -1,4 +1,32 @@
 import UIKit
 
 
-var greeting = "Hello, playground"
+// Mark - function to print a square
+func Square(sideLength: Int) {
+    for _ in 0..<sideLength {
+        var row = ""
+        for _ in 0..<sideLength {
+            row += "* "
+        }
+        print(row)
+    }
+}
+
+Square(sideLength: 8)
+
+
+
+
+// Mark - function to print a triangle
+func Triangle(height: Int) {
+    for i in 1...height {
+        var row = ""
+        for _ in 1...i {
+            row += "* "
+        }
+        print(row)
+    }
+}
+
+Triangle(height: 8)
+
