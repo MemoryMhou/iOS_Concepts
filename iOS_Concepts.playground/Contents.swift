29aba282a1a7cb1181f3b7d5f6c534be6901827d
MemoryMhou <memorymhou1@gmail.com>
2024-02-27 22:23:35 +0200

    simple loop statements

--- iOS_Concepts.playground/Contents.swift
+++ iOS_Concepts.playground/Contents.swift
@@ -1,4 +1,32 @@
 import UIKit
+//for in loop to print the sum of array
+
+var sum = 0
+for index in 20...50 { // we can replace index with underscore
+    sum += index
+    
+}
+print(sum)
 
 
-var greeting = "Hello, playground"
+
+// simple While loop to count down
+
+var counter = 5
+
+while counter > 0 {
+  print(counter)
+    counter -= 1
+}
+
+// simple While loop to count down
+var counters = -5
+
+while counters < -10 {
+  print(counters)
+    counters += 1
+}
+
+
+
+
